e2df233ac33fb2aa21cf632f9560a93c319caf84
Petar Ubavic <petar.ubavic@gmail.com>
2024-07-22 18:57:34 +0200

    Debug Probe

--- driver/kod.c
+++ driver/kod.c
@@ -197,7 +197,7 @@ static int __init fpu_init(void) {
     }
 */
     // Allocate coherent DMA buffer
-	tx_vir_buffer = dma_alloc_coherent(my_device, MAX_PKT_LEN, &tx_phy_buffer, GFP_DMA | GFP_KERNEL);
+	tx_vir_buffer = dma_alloc_coherent(NULL, MAX_PKT_LEN, &tx_phy_buffer, GFP_DMA | GFP_KERNEL);
 	printk(KERN_INFO "[fpu_init] Virtual and physical addresses coherent starting at %#x and ending at %#x\n", tx_phy_buffer, tx_phy_buffer+(uint)(MAX_PKT_LEN));
 	if(!tx_vir_buffer) {
 		printk(KERN_ALERT "[fpu_init] Could not allocate dma_alloc_coherent");
