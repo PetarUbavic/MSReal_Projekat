2487d4358a744739cfdc734ee46597b9136ae97a
Petar Ubavic <petar.ubavic@gmail.com>
2024-07-29 03:59:54 +0200

    Sredjujem DMA

--- driver/kod.c
+++ driver/kod.c
@@ -586,7 +586,6 @@ unsigned int dma_simple_write(dma_addr_t TxBufferPtr, unsigned int pkt_len, void
 	iowrite32((u32)TxBufferPtr, base_address + MM2S_SA_REG);
 	iowrite32(pkt_len, base_address + MM2S_LENGTH_REG);
 	printk(KERN_INFO "[dma_simple_write] Sent: %d \n", &TxBufferPtr);
-	printk(KERN_INFO "[dma_simple_write] Sent 2: %d \n", &(int*)(base_address + MM2S_SA_REG));
 	while(transaction_over0 == 1);
 	printk(KERN_INFO "[dma_simple_write] Successfully wrote in DMA \n");
 	//*tx_vir_buffer = ulazni_niz[cntrIn++];
