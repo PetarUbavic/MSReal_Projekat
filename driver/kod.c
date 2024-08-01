919ef14d34179beaeb57c2a91fbc6a411e20a8ad
Petar Ubavic <petar.ubavic@gmail.com>
2024-08-01 14:30:17 +0200

    Ne radi za vise od 1 clana, pokusaj ispravke

--- driver/kod.c
+++ driver/kod.c
@@ -522,7 +522,7 @@ ssize_t fpu_write(struct file *pfile, const char __user *buf, size_t length, lof
 			printk(KERN_INFO "[fpu_write] -----tx_phy_buffer: %#010x\n", tx_phy_buffer);
 			printk(KERN_INFO "[fpu_write] -----*tx_vir_buffer: %#010x\n", *tx_vir_buffer);
 			printk(KERN_INFO "[fpu_write] -----tx_vir_buffer: %#010x\n", tx_vir_buffer);
-			dma_simple_write(tx_phy_buffer, sizeof(ulazni_niz[pos]), dma_p->base_addr);
+			dma_simple_write(tx_phy_buffer, sizeof(ulazni_niz), dma_p->base_addr);
 		}
 	}
 
@@ -622,7 +622,7 @@ unsigned int dma_simple_write(dma_addr_t TxBufferPtr, unsigned int pkt_len, void
 	iowrite32(MM2S_DMACR_val, base_address + MM2S_DMACR_REG);
 	iowrite32((u32)TxBufferPtr, base_address + MM2S_SA_REG);
 	iowrite32(pkt_len, base_address + MM2S_LENGTH_REG);
-	//while(transaction_over0 == 1);
+	//while(transaction_over0 == 1);										// ovde puca kad se salje vise clanova, samo prvi put ide u interapt i vise ne
 	printk(KERN_INFO "[dma_simple_write] Sent: %#010x \n", TxBufferPtr);
 	printk(KERN_INFO "[dma_simple_write] Vrednost POSLE na adresi %p iznosi %#010x\n", tx_vir_buffer, *((unsigned int *)tx_vir_buffer));
 	printk(KERN_INFO "[dma_simple_write] Successfully wrote in DMA \n");
