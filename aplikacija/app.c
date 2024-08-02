182247e786114ea7cab56cd23c9f3ed4ba2cb3d8
Petar Ubavic <petar.ubavic@gmail.com>
2024-08-02 15:41:55 +0200

    Driver Radi, Pravim APP

--- aplikacija/app.c
+++ aplikacija/app.c
@@ -149,7 +149,8 @@ label1:    printf("Unesite broj - clanova niza: ");
             //printf("\n");
             printf("DEBUG --- VALUE je: %f \n", value);
             printf("DEBUG --- FloatToHex je: %#x \n", floatToHex(value));
-            tx_buffer[i] = floatToHex(value);
+            value = floatToHex(value);
+            tx_buffer[i] = value;
             printf("Na poziciji %d nalazi se vrednost: %#x\n", i, tx_buffer[i]);
         }
     }
