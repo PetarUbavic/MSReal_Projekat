f6ddfb85c32288cb7a9c6f7d83736b5230d04343
Petar Ubavic <petar.ubavic@gmail.com>
2024-08-09 16:46:25 +0200

    Opet MMAP radim

--- driver/kod.c
+++ driver/kod.c
@@ -487,8 +487,7 @@ ssize_t fpu_write(struct file *pfile, const char __user *buf, size_t length, lof
 			tx_phy_buffer += sizeof(uint);		// sluzi da prolazi kroz memoriju direktno, nisam uspeo preko vir da resim
 		}
 		for(pos = 0; pos < arr_size; pos++){
-			*tx_vir_buffer = fpu_array[pos];
-			tx_vir_buffer += sizeof(uint); 
+			tx_vir_buffer[pos] = fpu_array[pos];
 		}
 	}
 
